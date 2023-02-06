22c386e72b67daf94a0856d2f4cfe606a6c3c700
Brook <brooklinwai@gmail.com>
2023-02-06 14:48:35 +0800

    added content code for CSS and js files

---
+++ src/weather-script.js
@@ -0,0 +1,207 @@
+//thedate
+let theCurrentFullDate = new Date();
+
+//the hour
+function AmPmconvert() {
+  if (theCurrentFullDate.getHours() < 12) {
+    document.querySelector("#the-am-pm").innerHTML = "AM";
+  } else {
+    document.querySelector("#the-am-pm").innerHTML = "PM";
+  }
+}
+AmPmconvert();
+
+function digitalConvert() {
+  let theOldTimeHour = document.getElementById("theTimeHourDisplay");
+  let DigtalHourTime = [
+    "0",
+    "0",
+    "0",
+    "0",
+    "0",
+    "0",
+    "0",
+    "0",
+    "0",
+    "0",
+    "0",
+    "0",
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "11",
+    "12",
+  ];
+  if (theCurrentFullDate.getHours() > 11) {
+    theOldTimeHour.innerHTML = `${
+      DigtalHourTime[theCurrentFullDate.getHours()]
+    }`;
+  } else {
+    theOldTimeHour.innerHTML = `${theCurrentFullDate.getHours()}`;
+  }
+}
+digitalConvert();
+
+//the OG
+/*let theNewTimeHour = theCurrentFullDate.getHours();
+let theOldTimeHour = document.getElementById("theTimeHourDisplay");
+theOldTimeHour.innerHTML = `${theNewTimeHour}`;*/
+
+//the minuntes
+function FullMin() {
+  if (theCurrentFullDate.getMinutes() > 9) {
+    document.getElementById(
+      "theTimeMinDisplay"
+    ).innerHTML = `${theCurrentFullDate.getMinutes()}`;
+  } else
+    document.getElementById(
+      "theTimeMinDisplay"
+    ).innerHTML = `0${theCurrentFullDate.getMinutes()}`;
+}
+
+FullMin();
+
+//the OG
+//let theNewTimeMin = theCurrentFullDate.getMinutes();
+
+//let theOldTimeMin = document.getElementById("theTimeMinDisplay");
+//theOldTimeMin.innerHTML = `${theNewTimeMin}`;
+
+//The weekDays Displays
+let writtenDays = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let theNewWeekDay = writtenDays[theCurrentFullDate.getDay()];
+console.log(theNewWeekDay);
+
+let theOldWeekDay = document.getElementById("theWeekDayDisplay");
+theOldWeekDay.innerHTML = `${theNewWeekDay}`;
+
+//the tempurture
+/*function convertCeleus(event) {
+  event.preventDefault();
+  let mainTempDisplayC = document.querySelector("#Main-display-temp");
+  mainTempDisplayC.innerHTML = "23";
+}
+
+let celeus = document.querySelector("#Celeus-link");
+celeus.addEventListener("click", convertCeleus);
+
+function convertFarinhite(event) {
+  event.preventDefault();
+  let mainTempDisplayF = document.querySelector("#Main-display-temp");
+  mainTempDisplayF.innerHTML = "73";
+}
+let farinhite = document.querySelector("#Farinhite-link");
+farinhite.addEventListener("click", convertFarinhite);*/
+
+//Search Form with Live Data input
+function liveDataInputDisplay(response) {
+  let mainTitleDisplay = document.querySelector(
+    "h1#main-title-display-search-city"
+  );
+  let theMainTemp = document.querySelector("#Main-display-temp");
+  let liveWindMainDisplay = document.querySelector("#the-main-wind-speed");
+  let livePrecipitionMsinDisplay = document.querySelector(
+    "#the-main-precipition"
+  );
+
+  mainTitleDisplay.innerHTML = `${response.data.name}`;
+  theMainTemp.innerHTML = Math.floor(response.data.main.temp);
+  liveWindMainDisplay.innerHTML = Math.floor(response.data.wind.speed);
+  livePrecipitionMsinDisplay.innerHTML = Math.floor(
+    response.data.main.humidity
+  );
+}
+
+//weather display
+function findGeoWeatherLocation(position) {
+  let inputSearchDisplayTitle = document.querySelector("input#form1");
+
+  if (inputSearchDisplayTitle.value) {
+    axios
+      .get(
+        `${apiWeather}q=${inputSearchDisplayTitle.value}&appid=${apiKey}&units=metric`
+      )
+      .then(liveDataInputDisplay);
+  } else {
+    axios
+      .get(
+        `${apiWeather}lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`
+      )
+      .then(liveDataInputDisplay);
+  }
+}
+
+let inputSearch = document.querySelector("button#submit-form-button");
+inputSearch.addEventListener("click", findGeoWeatherLocation);
+
+navigator.geolocation.getCurrentPosition(findGeoWeatherLocation);
+let apiWeather = "https://api.openweathermap.org/data/2.5/weather?";
+let apiKey = "2b6fdad0cbd018949c50c70f72250726";
+
+/* 
+function tempGeo(position) {
+  axios
+    .get(
+      `${apiWeather}lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`
+    )
+    .then(display);
+    let apiWeather = "https://api.openweathermap.org/data/2.5/weather?q=Sydney";
+
+navigator.geolocation.getCurrentPosition(tempGeo);*/
+//function liveDataInput(response) {}
+/*
+let apiWeather = "https://api.openweathermap.org/data/2.5/weather?";
+let apiKey = "2b6fdad0cbd018949c50c70f72250726";
+let city = document.querySelector("input#form1");
+console.log(city);*/
+/*axios
+  .get(`${apiWeather}q=${city}&appid=${apiKey}&units=metric`)
+  .then(liveDataInput)*/
+
+/*
+function serchInputDisplay() {
+  let inputSearchDisplayTitle = document.querySelector("input#form1");
+
+  if (inputSearchDisplayTitle.value) {
+    axios
+      .get(
+        `${apiWeather}q=${inputSearchDisplayTitle.value}&appid=${apiKey}&units=metric`
+      )
+      .then(liveDataInputDisplay);
+  } else {
+    alert("Please enter a city");
+  }
+}
+*/
+
+/*
+function serchInputDisplay() {
+  let inputSearchDisplayTitle = document.querySelector("input#form1");
+
+  if (inputSearchDisplayTitle.value) {
+    axios
+      .get(
+        `${apiWeather}q=${inputSearchDisplayTitle.value}&appid=${apiKey}&units=metric`
+      )
+      .then(liveDataInputDisplay);
+  } else {
+    alert("Please enter a city");
+  }
+}
+*/

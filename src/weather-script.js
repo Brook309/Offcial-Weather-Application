e94b92e68a381531457063a73c0e0e0f930d5d65
Brook <brooklinwai@gmail.com>
2023-02-22 01:01:54 +0800

    updated the digital hours to be a bit better and rolled out the kinks of it. Displaying 0 then 12 am

--- src/weather-script.js
+++ src/weather-script.js
@@ -15,18 +15,6 @@ function digitalConvert() {
   let theOldTimeHour = document.getElementById("theTimeHourDisplay");
   let DigtalHourTime = [
     "12",
-    "0",
-    "0",
-    "0",
-    "0",
-    "0",
-    "0",
-    "0",
-    "0",
-    "0",
-    "0",
-    "0",
-    "12",
     "1",
     "2",
     "3",
@@ -39,14 +27,19 @@ function digitalConvert() {
     "10",
     "11",
     "12",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "11",
   ];
-  if (theCurrentFullDate.getHours() > 11) {
-    theOldTimeHour.innerHTML = `${
-      DigtalHourTime[theCurrentFullDate.getHours()]
-    }`;
-  } else {
-    theOldTimeHour.innerHTML = `${theCurrentFullDate.getHours()}`;
-  }
+  theOldTimeHour.innerHTML = `${DigtalHourTime[theCurrentFullDate.getHours()]}`;
 }
 digitalConvert();
 
@@ -57,20 +50,55 @@ theOldTimeHour.innerHTML = `${theNewTimeHour}`;*/
 
 //the minuntes
 function FullMin() {
+  let updatetime = theCurrentFullDate.getMinutes();
   if (theCurrentFullDate.getMinutes() > 9) {
-    document.getElementById(
-      "theTimeMinDisplay"
-    ).innerHTML = `${theCurrentFullDate.getMinutes()}`;
+    document.getElementById("theTimeMinDisplay").innerHTML = updatetime;
   } else
     document.getElementById(
       "theTimeMinDisplay"
     ).innerHTML = `0${theCurrentFullDate.getMinutes()}`;
 }
-
 FullMin();
 
 //the OG
+
 //let theNewTimeMin = theCurrentFullDate.getMinutes();
+/*function currentTime() {
+  let date = new Date();
+  let hh = date.getHours();
+  let mm = date.getMinutes();
+  let ss = date.getSeconds();
+  let session = "AM";
+
+  if (hh === 0) {
+    hh = 12;
+  }
+  if (hh > 12) {
+    hh = hh - 12;
+    session = "PM";
+  }
+
+  hh = hh < 10 ? "0" + hh : hh;
+  mm = mm < 10 ? "0" + mm : mm;
+  ss = ss < 10 ? "0" + ss : ss;
+
+  let time = hh + ":" + mm + ":" + ss + " " + session;
+
+  document.getElementById("clock").innerText = time;
+  let t = setTimeout(function () {
+    currentTime();
+  }, 1000);
+}
+
+currentTime();
+/*console.log(display_ct);
+function display_ct() {
+  var x = new Date();
+  var x1 = x.getMonth() + 1 + "/" + x.getDate() + "/" + x.getFullYear();
+  x1 = x1 + " - " + x.getHours() + ":" + x.getMinutes() + ":" + x.getSeconds();
+  document.getElementById("theTimeMinDisplay").innerHTML = x1;
+  display_ct();
+}*/
 
 //let theOldTimeMin = document.getElementById("theTimeMinDisplay");
 //theOldTimeMin.innerHTML = `${theNewTimeMin}`;
